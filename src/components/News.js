d87a10044ad3596703d940e826e641cb3034dfa8
AVNISH KUMAR <avnishkrsharma3@gmail.com>
2022-06-22 18:32:26 +0530

    previous to laoding bar at navbar

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,105 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 export default class News extends Component {
-  render() {
-    return (
-      <div>This is a News components
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        console.log("constructor");
+        this.state = {
+            articles: [],
+            loading: true,
+            page: 1,
+            title: "",
+            totalResults: 0,
+        };
+        document.title = this.capitalizeFirstLetter(`${props.category}`) + ' - Latest News'
+        console.log(document.title);
+    }
+    capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    static defaultProps = {
+        country: 'in',
+        pageSize: 6,
+        category: 'general'
+    }
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+        cnt: PropTypes.number
+    }
+    async componentDidMount() {
+        //console.log("mount");
+        let url = `https://newsapi.org/v2/top-headlines?country=In&category=${this.props.category}&apiKey=db93ee39ecd94a85bd80f1207b1e0ef6&page=${this.state.page}&pagesize=${this.props.pageSize}`;
+        this.setState({
+            loading : true
+        });
+        //console.log(url);
+        let data = await fetch(url);
+        let parseData = await data.json();
+        //console.log(parseData);
+            this.setState({
+            articles : parseData.articles, // it is an array
+            totalResults : parseData.totalResults,
+            loading :  false});
+        // console.log(this.state.page);
+        // console.log(this.state.articles);
+    }
+    fetchMoreData = async() => {
+        console.log("fetch Data");
+        this.state.page = this.state.page + 1;       
+        //console.log(this.state.page);
+        const url = `https://newsapi.org/v2/top-headlines?country=In&category=${this.props.category}&apiKey=db93ee39ecd94a85bd80f1207b1e0ef6&page=${this.state.page}&pagesize=${this.props.pageSize}`;
+        //console.log(url);
+        this.setState({
+            loading: true
+        })
+        let data = await fetch(url);
+        let parseData = await data.json();
+        //console.log(url);
+        this.setState({
+            articles: this.state.articles.concat(parseData.articles), // it is an array
+            loading: false,
+            totalResults : parseData.totalResults,
+        });
+    };
+    render() {
+        //console.log("render");
+        return (
+            <>
+                <h1 className="text-center" style={{ marginTop: "25px", marginBottom: "15px" }}> {this.capitalizeFirstLetter(this.props.category) === "General" ? "News" : this.capitalizeFirstLetter(this.props.category)} - Top Headlines</h1>
+                {this.state.loading && <Spinner/>}
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData} 
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner/>}
+                >
+            <div className="container">
+                <div className="row">
+                    {this.state.articles.map((elem) => {
+                        return (
+                            <div key={elem.url} className="col-md-4">
+                                <NewsItem
+                                    title={(elem.title) ? elem.title.slice(0, 73) : ""}
+                                    description={(elem.description) ? elem.description.slice(0, 88) : ""}
+                                    imageUrl={elem.urlToImage}
+                                    newsUrl={elem.url}
+                                    author={elem.author}
+                                    date={elem.publishedAt}
+                                    source={elem.source.name}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+                </InfiniteScroll>
+            </>
+        );
+    }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,9 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-  render() {
-    return (
-      <div>This is a NewsItem</div>
-    )
-  }
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left :'91%', zIndex : '1'}}>
+                        {source}
+                    </span>
+                    <img
+                        src={
+                            !imageUrl
+                                ? "https://images.news18.com/ibnlive/uploads/2022/06/star-165553670816x9.jpg"
+                                : imageUrl
+                        }
+                        alt="..."
+                    />
+                    <div className="card-body">
+                        <h5 className="card-title" >{title}...</h5>
+                        <p className="card-text">{description}.... </p>
+                        <p className="card-text">
+                            <small className="text-muted">
+                                By {!author ? "unknown" : author}, Last updated{" "}
+                                <b>{new Date(date).toGMTString()}</b>.
+                            </small>
+                        </p>
+                        <a
+                            href={newsUrl}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn btn-sm btn-primary"
+                        >
+                            Read More
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
