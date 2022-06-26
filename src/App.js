9a26309539e0d03499d175586e7118f880f09929
AVNISH KUMAR <avnishkrsharma3@gmail.com>
2022-06-26 19:48:37 +0530

    nav bar cross icon added

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
 
 import './App.css';
 import NavBar from './components/NavBar'
-import React, { Component } from 'react'
+import React from 'react'
 import  News from './components/News'
 import LoadingBar from 'react-top-loading-bar';
+import { useState } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
-export default class App extends Component {
-  pageSize = 9;
-  constructor() {
-  super()
-  this.state = {
-    progress : 0,
-    apiKey : "2e6fb912780e47a989d46d11404b69cc"
-  }
-  }
-  setProgress = (progress) => {
-    this.setState({progress: progress})
-  }
-  render() {
+const  App = () => {
+  const [pageSize] = useState(5);
+  const [progress, setProgress] = useState(0);
+  const [apiKey] = useState("2e6fb912780e47a989d46d11404b69cc");
     return (
       <div>
       <Router>
       <LoadingBar
         color='#f11946'
-        progress={this.state.progress} 
+        progress={progress} 
         height = {3}
       />
         <NavBar/>
         <Routes>
-          <Route exact path="/" element = {<News setProgress = {this.setProgress} apiKey = {this.state.apiKey}  key="general" pageSize={this.pageSize} country="in" category={'general'}/>}/>
-          <Route exact path="/business" element = {<News setProgress = {this.setProgress} apiKey = {this.state.apiKey} key="business" pageSize={this.pageSize} country="in" category={'business'}/>}/>
-          <Route exact path="/entertainment" element = {<News setProgress = {this.setProgress} apiKey = {this.state.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category={'entertainment'}/>}/>
-          <Route exact path="/health" element = {<News setProgress = {this.setProgress} apiKey = {this.state.apiKey} key="health" pageSize={this.pageSize} country="in" category={'health'}/>}/>
-          <Route exact path="/sports"element = {<News setProgress = {this.setProgress} apiKey = {this.state.apiKey} key="sports" pageSize={this.pageSize} country="in" category={'sports'}/>}/>
-          <Route exact path="/science"element = {<News setProgress = {this.setProgress} apiKey = {this.state.apiKey} key="science" pageSize={this.pageSize} country="in" category={'science'}/>}/>
-          <Route exact path="/technology"element = {<News setProgress = {this.setProgress} apiKey = {this.state.apiKey} key="technology" pageSize={this.pageSize} country="in" category={'technology'}/>}/>
+          <Route exact path="/" element = {<News setProgress = {setProgress} apiKey = {apiKey}  key="general" pageSize={pageSize} country="in" category={'general'}/>}/>
+          <Route exact path="/business" element = {<News setProgress = {setProgress} apiKey = {apiKey} key="business" pageSize={pageSize} country="in" category={'business'}/>}/>
+          <Route exact path="/entertainment" element = {<News setProgress = {setProgress} apiKey = {apiKey} key="entertainment" pageSize={pageSize} country="in" category={'entertainment'}/>}/>
+          <Route exact path="/health" element = {<News setProgress = {setProgress} apiKey = {apiKey} key="health" pageSize={pageSize} country="in" category={'health'}/>}/>
+          <Route exact path="/sports"element = {<News setProgress = {setProgress} apiKey = {apiKey} key="sports" pageSize={pageSize} country="in" category={'sports'}/>}/>
+          <Route exact path="/science"element = {<News setProgress = {setProgress} apiKey = {apiKey} key="science" pageSize={pageSize} country="in" category={'science'}/>}/>
+          <Route exact path="/technology"element = {<News setProgress = {setProgress} apiKey = {apiKey} key="technology" pageSize={pageSize} country="in" category={'technology'}/>}/>
         </Routes>
       </Router>
       </div>
     )
-  }
 }
-
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
+import "./NavBar.css";
 import {  Link  } from "react-router-dom";
 const NavBar = () => {
     
         return (
             <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" >
                 <div className="container-fluid">
-                    <Link className="navbar-brand"  to="/" >MyNewsWebsite</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <Link className="navbar-brand"  to="/" >Gorilla News 🌍</Link>
+                    <button className="navbar-toggler collapsed border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
+                        <div className='close-icon py-1'>✗</div>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            
                             <li className="nav-link">
                                 <Link className ="nav-link" to="/">Home</Link>
                             </li>
